7226baa309d5b5d4b7aed03e3643fe063d37cc21
Brian <brian.holland.pr@gmail.com>
2021-03-15 18:59:54 -0700

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+import Fizzbuzz from './Fizzbuzz';
+
+const Counter = () => {
+	const [ count, setCount ] = useState(0);
+
+	function onClickHandler() {
+		setCount(count - 1);
+	}
+
+	return (
+		<div className="main-display">
+			<h1>FizzBuzz Counter</h1>
+			<br />
+			<div className="abs-fb">
+				<Fizzbuzz number={count} />
+			</div>
+
+			<p className="count-display">The current count is: {count}</p>
+			<div>
+				<button className="count-button" onClick={onClickHandler}>
+					-1
+				</button>
+				<span> Count </span>
+				<button className="count-button" onClick={() => setCount(count + 1)}>
+					+1
+				</button>
+			</div>
+
+			<button className="reset-button" onClick={() => setCount(0)}>
+				Reset
+			</button>
+		</div>
+	);
+};
+
+export default Counter;
---
+++ src/components/Fizzbuzz.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Fizzbuzz = ({ number }) => {
+	function calcFizz(num) {
+		if (num === 0) {
+			return;
+		}
+		if (num % 5 === 0 && num % 3 === 0) {
+			return `FizzBuzz!`;
+		} else if (number % 3 === 0) {
+			return `Fizz`;
+		} else if (number % 5 === 0) {
+			return `Buzz`;
+		}
+	}
+
+	return <span className={number % 3 === 0 && number % 5 === 0 && `bold-display`}>{calcFizz(number)}</span>;
+};
+
+export default Fizzbuzz;
